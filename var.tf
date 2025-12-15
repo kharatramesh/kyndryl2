34a5f91f7be0ff4942d90c17b37dece77ef01a47
ramesh <ramesh@example.com>
2025-12-15 12:31:10 +0530

    change3

--- var.tf
+++ var.tf
@@ -2,5 +2,13 @@ variable "sid" { type = string }
 variable "cid" { type = string }
 variable "cs" { type = string }
 variable "tid" { type = string }
-variable "rgname" { type = string }
-variable "location" { type = string }
+
+variable "rgname" { 
+    type = string 
+    default = "kyndryl-rg" 
+}
+
+variable "location" { 
+    type = string 
+    default = "East US" 
+}
