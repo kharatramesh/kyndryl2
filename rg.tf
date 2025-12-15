6aa913b5e1b84ed63bd56c6c29404bf279c6ac24
ramesh <ramesh@example.com>
2025-12-15 12:22:34 +0530

    change2

--- rg.tf
+++ rg.tf
@@ -2,3 +2,8 @@ resource "azurerm_resource_group" "rg" {
   name     = var.rgname
   location = var.location
 }
+
+resource "azurerm_resource_group" "rg1" {
+  name     = "test1"
+  location = var.location
+}
